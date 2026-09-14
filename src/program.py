60d4c1f22bbe577409fdd99a15d101c8aa718975
edwinsujo <edwinsujo@gmail.com>
2026-09-14 16:44:53 +0530

    main

--- src/program.py
+++ src/program.py
@@ -56,8 +56,8 @@ skilllist = [
     "Java", "Python", "C++", "JavaScript", "C#", "PHP", "Ruby", "Go", "Swift", "Kotlin",
     "HTML", "CSS", "React", "Angular", "Vue.js", "Node.js", "Apache", "Nginx",
     "MySQL", "Oracle", "PostgreSQL", "MongoDB", "Cassandra", "Redis",
-    "AWS", "Azure", "GCP","Bitcoin","Ethereum","Solidity","C"
-    "Git", "version control systems", "Jenkins", "GitLab CI/CD","AUTO CAD","Fanuc Series"
+    "AWS", "Azure", "GCP","Bitcoin","Ethereum","Solidity","C",
+    "Git", "version control systems", "Jenkins", "GitLab CI/CD","AUTO CAD","Fanuc Series",
     "TCP/IP", "network protocols", "routing", "firewalls",
     "Network security principles", "encryption", "firewalls", "IDS",
     "Windows (Server and Desktop)", "Linux (Ubuntu, Red Hat, CentOS)", "macOS",
